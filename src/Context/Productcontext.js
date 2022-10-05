a0b2f7d8624481935e0c41ab5da12b4f94f93b78
madhuboora930 <madhu.boora.28@gmail.com>
2022-10-05 23:59:20 +0530

    Fatch API data use axiousv13

--- src/Context/Productcontext.js
+++ src/Context/Productcontext.js
@@ -1,23 +1,40 @@
 
 
-// create a context
-// provider
-// consumer =>useContext Hook
-import { createContext } from "react";
-import { useContext } from "react";
+
+import { createContext, useContext, useEffect } from "react";
+import axios from "axios";
+
 const AppContext = createContext();
 
+const API = "https://api.pujakaitem.com/api/products";
+
 const AppProvider = ({ children }) => {
+  const getProducts = async (url) => {
+    
+      const res = await axios.get(url);
+      const products = await res.data;
+      console.log(
+        "🚀 ~ file: productcontex.js ~ line 18 ~ getProducts ~ products",
+        products
+      );
+      }
+
+      
+  useEffect(() => {
+    getProducts(API);
+  }, []);
+
   return (
+    <>
     <AppContext.Provider value={{ myName: "Madhu Boora" }}>
       {children}
     </AppContext.Provider>
+    
+    </>
   );
 };
-
 // custom hooks
 const useProductContext = () => {
-  return useContext(AppContext);
-};
-
-export { AppProvider, AppContext, useProductContext };
+    return useContext(AppContext);
+  };
+export{AppProvider,AppContext,useProductContext}
